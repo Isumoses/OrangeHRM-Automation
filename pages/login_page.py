a9e628a9c6552224a08f6f7534474e4589d3d5ce
Isumoses <mosesamanda0@gmail.com>
2025-04-22 19:49:55 +0530

    Time.sleep Added

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
+import time
 
 class LoginPage(BasePage):
     URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
@@ -11,11 +12,15 @@ class LoginPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
+        time.sleep(2)
 
     def load(self):
         self.driver.get(self.URL)
 
     def login(self, username="Admin", password="admin123"):
         self.wait.until(EC.presence_of_element_located(self.USERNAME_FIELD)).send_keys(username)
+        time.sleep(1)
         self.driver.find_element(*self.PASSWORD_FIELD).send_keys(password)
+        time.sleep(1)
         self.driver.find_element(*self.LOGIN_BUTTON).click()
+        time.sleep(2)
--- tests/test_orangehrm.py
+++ tests/test_orangehrm.py
@@ -3,6 +3,7 @@ import pytest
 from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
 from pages.leave_page import LeavePage
+import time
 
 def test_home_page_title(driver):
     login_page = LoginPage(driver)
@@ -12,9 +13,12 @@ def test_home_page_title(driver):
 def test_login_functionality(driver):
     login_page = LoginPage(driver)
     login_page.load()
+    time.sleep(1)
     login_page.login()
+    time.sleep(2)
     dashboard_page = DashboardPage(driver)
     assert dashboard_page.is_loaded(), "Dashboard page did not load."
+    time.sleep(2)
 
 def test_leave_functionality(driver):
     login_page = LoginPage(driver)
@@ -30,6 +34,7 @@ def test_logout_functionality(driver):
     login_page = LoginPage(driver)
     login_page.load()
     login_page.login()
+    time.sleep(2)
 
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support.ui import WebDriverWait
@@ -39,8 +44,10 @@ def test_logout_functionality(driver):
 
     user_dropdown = wait.until(EC.element_to_be_clickable((By.XPATH, '//span[@class="oxd-userdropdown-tab"]')))
     user_dropdown.click()
+    time.sleep(1)
 
     logout_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[text()="Logout"]')))
     logout_button.click()
+    time.sleep(2)
 
     assert "login" in driver.current_url
